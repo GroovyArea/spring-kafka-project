9f42b820105f3f07af3d72002917f3f0dc4f38aa
GroovyArea <095201a@naver.com>
2023-04-17 23:56:14 +0900

    feat : project init

--- spring-kafka-producer-with-rest-controller/src/main/java/com/pipeline/SpringKafkaProducerWithRestControllerApplication.java
+++ spring-kafka-producer-with-rest-controller/src/main/java/com/pipeline/SpringKafkaProducerWithRestControllerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringKafkaProjectApplication {
+public class SpringKafkaProducerWithRestControllerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringKafkaProjectApplication.class, args);
+        SpringApplication.run(SpringKafkaProducerWithRestControllerApplication.class, args);
     }
 
 }
